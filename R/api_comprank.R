4ef04c4e4ef6f588d01a97f522f194a895ce38b5
Simon Roth <nomis.roth@gmx.net>
2020-03-04 12:05:58 +0100

    own h2h function

--- R/api_comprank.R
+++ R/api_comprank.R
@@ -1,7 +1,34 @@
+#' num_wins
+#' @export
+near <- function (x, y, tol = .Machine$double.eps^0.5) {
+  abs(x - y) < tol
+}
+
+#' num_wins
+#' @export
+num_wins <- function (score_1, score_2, half_for_draw = F, na.rm = T){
+  near_score <- deeplyr::near(score_1, score_2)
+  sum(score_1[!near_score] > score_2[!near_score], na.rm = na.rm) + half_for_draw * 0.5 * sum(near_score, na.rm = na.rm)
+}
+
+#' my_h2h_funs
+#' @export
+my_h2h_funs <- list(
+  mean_score_diff = expr(mean(score1 - score2)), 
+  mean_score_diff_pos = expr(max(mean(score1 - score2), 0)), 
+  mean_score = expr(mean(score1)), 
+  sum_score_diff = expr(sum(score1 - score2)), 
+  sum_score_diff_pos = expr(max(sum(score1 - score2), 0)), 
+  sum_score = expr(sum(score1)), 
+  num_wins = expr(deeplyr::num_wins(score1, score2, half_for_draw = F)), 
+  num_wins2 = expr(deeplyr::num_wins(score1, score2, half_for_draw = T))
+)
+
 #' get_comprank_h2h
 #' @export
 get_comprank_h2h <- function(wide){
-  comperes::h2h_long(wide, !!!comperes::h2h_funs) %>%
+  
+  comperes::h2h_long(wide, !!!deeplyr::my_h2h_funs) %>%
     dplyr::select(-player2) %>%
     dplyr::group_by(player1) %>%
     dplyr::summarise_all(mean, na.rm = T) %>%
@@ -31,15 +58,15 @@ fit_comperank <- function(self){
   comperes::as_widecr()
   long <- comperes::as_longcr(wide)
 
+  h2h <- get_comprank_h2h_pos(wide)
   massy <- comperank::rate_massey(wide) 
   colley <- comperank::rate_colley(wide)
-  keener <- comperank::rate_keener(long, !!! comperes::h2h_funs["mean_score"])
-  markov <- comperank::rate_markov(long, !!! comperes::h2h_funs["mean_score"])
+  keener <- comperank::rate_keener(long, !!! deeplyr::my_h2h_funs["mean_score"])
+  markov <- comperank::rate_markov(long, !!! deeplyr::my_h2h_funs["mean_score"])
   od <- comperank::rate_od(long, if (player1[1] == player2[1]) 0 else mean(score1))
   elo <- comperank::rate_elo(long)
-  h2h <- get_comprank_h2h_pos(wide)
   
-  list(massy, colley, keener, markov, od, elo, h2h) %>%
+  list(massy, colley, keener, markov, od, elo,h2h) %>%
     purrr::compact() %>%
     purrr::reduce(dplyr::inner_join, by = "player") %>%
     dplyr::mutate_all(round, 3)
